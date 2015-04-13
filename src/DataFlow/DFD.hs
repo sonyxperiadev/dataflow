2233dece51a5a97b4b5f1540040d8565bb73b21f
Oskar Wickström <oskar.wickstrom@live.com>
2015-04-13 20:00:06 +0200

    Layout restore after refactoring

--- src/DataFlow/DFD.hs
+++ src/DataFlow/DFD.hs
@@ -8,28 +8,35 @@ import DataFlow.Graphviz.EdgeNormalization
 inQuotes :: String -> String
 inQuotes s = "\"" ++ s ++ "\""
 
+inAngleBrackets :: String -> String
+inAngleBrackets s = "<" ++ s ++ ">"
+
 label :: String -> Attr
-label s = Attr (ID "label") (ID $ "<" ++ s ++ ">")
+label s = Attr (ID "label") (ID $ inAngleBrackets s)
 
 bold :: String -> String
 bold s = "<b>" ++ s ++ "</b>"
 
 convertObject :: C.Object -> StmtList
+
 convertObject (C.InputOutput id' name) = [
-  NodeStmt (ID id') [
-    Attr (ID "shape") (ID "square"),
-    Attr (ID "style") (ID "bold"),
-    label $ bold name
-  ]]
+    NodeStmt (ID id') [
+      Attr (ID "shape") (ID "square"),
+      Attr (ID "style") (ID "bold"),
+      label $ bold name
+    ]
+  ]
+
 convertObject (C.TrustBoundary id' name objects) =
   let sgId = (ID $ "cluster_" ++ id')
       objectStmts = convertObjects objects
       sgAttrStmt = AttrStmt Graph [
-        Attr (ID "fontsize") (ID "10"),
-        Attr (ID "fontcolor") (ID "grey30"),
-        Attr (ID "style") (ID "dashed"),
-        Attr (ID "color") (ID "grey30"),
-        label $ bold name]
+          Attr (ID "fontsize") (ID "10"),
+          Attr (ID "fontcolor") (ID "grey30"),
+          Attr (ID "style") (ID "dashed"),
+          Attr (ID "color") (ID "grey30"),
+          label $ bold name
+        ]
       stmts = sgAttrStmt : objectStmts
   in [SubgraphStmt $ Subgraph sgId stmts]
 
@@ -40,6 +47,7 @@ convertObject (C.Function id' name) =
       label $ bold name
     ]
   ]
+
 convertObject (C.Database id' name) =
   [
     NodeStmt (ID id') [
@@ -47,21 +55,45 @@ convertObject (C.Database id' name) =
       label $ printf "<table sides=\"TB\" cellborder=\"0\"><tr><td>%s</td></tr></table>" (bold name)
     ]
   ]
+
 convertObject (C.Flow i1 i2 op desc) =
   [
     EdgeStmt (EdgeExpr (IDOperand (NodeID (ID i1) Nothing))
                        Arrow
                        (IDOperand (NodeID (ID i2) Nothing))) [
-      Attr (ID "shape") (ID "none"),
       label $ bold op ++ "<br/>" ++ desc
     ]
   ]
 
 convertObjects :: [C.Object] -> StmtList
-convertObjects = concat . (map convertObject)
+convertObjects = concatMap convertObject
+
+defaultGraphStmts :: StmtList
+defaultGraphStmts = [
+    AttrStmt Graph [
+      Attr (ID "fontname") (ID "Arial"),
+      Attr (ID "fontsize") (ID "13")
+    ],
+    AttrStmt Node [
+      Attr (ID "fontname") (ID "Arial"),
+      Attr (ID "fontsize") (ID "11")
+    ],
+    AttrStmt Edge [
+      Attr (ID "shape") (ID "none"),
+      Attr (ID "fontname") (ID "Arial"),
+      Attr (ID "fontsize") (ID "11")
+    ],
+    EqualsStmt (ID "labelloc") (ID $ inQuotes "t"),
+    EqualsStmt (ID "fontsize") (ID "20"),
+    EqualsStmt (ID "nodesep") (ID "1"),
+    EqualsStmt (ID "rankdir") (ID "t")
+  ]
 
 asDFD :: C.Diagram -> Graph
 asDFD (C.Diagram (Just name) objects) =
-  normalize $ Digraph (ID $ inQuotes name) (convertObjects objects)
+  normalize $ Digraph (ID $ inQuotes name) $
+    let lbl = EqualsStmt (ID "label") (ID $ inAngleBrackets $ bold name)
+    in lbl : defaultGraphStmts ++ convertObjects objects
 asDFD (C.Diagram Nothing objects) =
-  normalize $ Digraph (ID $ "Untitled") (convertObjects objects)
+  normalize $ Digraph (ID "Untitled") $
+    defaultGraphStmts ++ convertObjects objects
