bf3e3f860db12ce840f41642bb0898b8ff3f6dbd
Oskar Wickström <oskar.wickstrom@live.com>
2015-08-05 16:13:38 +0200

    Add support for templates

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,31 +1,50 @@
 module Main where
 
+import System.IO
 import System.Environment
+import System.Exit
 import DataFlow.Reader
 import DataFlow.DFD
 import DataFlow.SequenceDiagram
 import DataFlow.Graphviz.Renderer
 import DataFlow.PlantUML.Renderer
+import DataFlow.Hastache.Renderer
+import qualified Data.Text.Lazy.IO as TL
 
-data Command = DFD | Lint
-
-usage :: String
-usage = "Usage: dataflow (dfd|seq) FILE"
+usage :: IO ()
+usage = hPutStrLn stderr $ unlines [
+    "Usage: dataflow command args*",
+    "",
+    "Commands",
+    "--------",
+    "dfd SRC               - outputs a DFD in the Graphviz DOT format",
+    "seq SRC               - outputs a sequence diagram in PlantUML format",
+    "template TEMPLATE SRC - renders the TEMPLATE using data from SRC",
+    "",
+    "All commands print to stdout."
+  ]
 
 dfd :: FilePath -> IO ()
 dfd path = do
   res <- readDiagramFile path
   case res of
     (Left err) -> print err
-    (Right diagram) -> putStr $ renderGraphviz $ asDFD diagram
+    (Right d) -> putStr $ renderGraphviz $ asDFD d
 
 seq' :: FilePath -> IO ()
 seq' path = do
   res <- readDiagramFile path
   case res of
     (Left err) -> print err
-    (Right diagram) -> putStr $ renderPlantUML $ asSequenceDiagram diagram
+    (Right d) -> putStr $ renderPlantUML $ asSequenceDiagram d
 
+template :: FilePath -> FilePath -> IO ()
+template tmplPath path = do
+  res <- readDiagramFile path
+  tmplStr <- readFile tmplPath
+  case res of
+    (Left err) -> print err
+    (Right d) -> renderTemplate tmplStr path d >>= TL.putStr
 
 main :: IO ()
 main = do
@@ -33,4 +52,7 @@ main = do
   case args of
     ["dfd", path] -> dfd path
     ["seq", path] -> seq' path
-    _ -> fail usage
+    ["template", tmplPath, path] -> template tmplPath path
+    ["--help"] -> usage
+    _ -> do hPutStrLn stderr "Invalid command!\n\nRun with --help to see usage."
+            exitWith $ ExitFailure 1
